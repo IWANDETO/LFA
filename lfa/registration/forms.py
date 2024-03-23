ae9a6e8f12a879fcbb362875b54ebfe8515b0142
IWANDETO <wandettoian@gmail.com>
2024-03-23 14:09:48 +0300

    Fixed Widget Error

--- lfa/registration/forms.py
+++ lfa/registration/forms.py
@@ -5,4 +5,4 @@ This is a Login Form used to Authenticate users against the database
 """
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(widgets=forms.PasswordInput) # Password Input Widget renders HTML password Element
+    password = forms.CharField(widget=forms.PasswordInput) # Password Input Widget renders HTML password Element
