405c9a8fbc7d4f0b40be50a1f6c288fff7cab76b
IWANDETO <wandettoian@gmail.com>
2024-03-29 19:35:08 +0300

    Added Fingerprint Input in Forms

--- lfa/account/forms.py
+++ lfa/account/forms.py
@@ -6,3 +6,6 @@ This is a Login Form used to Authenticate users against the database
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput) # Password Input Widget renders HTML password Element
+    fingerprint = forms.CharField( max_length=128, blank=True, null=True) # Fingerprint Input to store User Fingerprints
+
+    
